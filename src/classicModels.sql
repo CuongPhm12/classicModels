97d1ec49510d265e7207263065c63b940b095bb3
unknown <phamvietcuong12@gmail.com>
2021-11-03 15:51:36 +0700

    thuc hanh store procedure

--- src/classicModels.sql
+++ src/classicModels.sql
@@ -10,3 +10,19 @@ explain select * from customers where customerName ='Land of Toys Inc.';
 alter table customers add index idx_full_name(contactFirstName,contactLastName);
 explain select * from customers where contactFirstName = 'Jean' or contactFirstName = 'King';
 
+create procedure fillAllCustomers()
+begin
+    select * from customers;
+end;
+
+
+call fillAllCustomers();
+
+drop procedure if exists `fillAllCustomers`;
+
+create procedure fillAllCustomer()
+begin
+    select * from customers where customerNumber = 175;
+end;
+
+call fillAllCustomer();
